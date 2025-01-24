74a8ee49ac9145b564845bef8527e85c44d977dd
Alexander Zellner <alex.zellner@web.de>
2025-01-24 20:20:58 +0100

    add argc validation

--- src/main.c
+++ src/main.c
@@ -3,11 +3,31 @@
 //just for debugging, remove later
 #include <stdio.h>
 
+
+int valid_input_size(int size)
+{
+	// valid sizes are multile of 4's. 
+	// starting at a 4x4 matrix
+	
+	// to small. min 4x4
+	if (size < 16)
+		return (0);
+
+	// to big. max. 9x9
+	if (size > 36)
+		return(0);
+
+	// no multiple of 4
+	if (size % 4 != 0)
+		return (0);
+
+	return (1);
+}
+
 int	main(int argc, char **argv)
 {
 	
-
-	if (argc != 17)
+	if (!valid_input_size(argc-1))
 	{
 		write(1, "Error\n", 6);
 	}

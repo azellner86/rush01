1d579341e6a128e6da45516c2b22d39a971f39cb
Alexander Zellner <alex.zellner@web.de>
2025-01-24 20:07:50 +0100

    add main.c and makefile

---
+++ src/main.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+//just for debugging, remove later
+#include <stdio.h>
+
+int	main(int argc, char **argv)
+{
+	
+
+	if (argc != 17)
+	{
+		write(1, "Error\n", 6);
+	}
+
+	return (0);
+}
